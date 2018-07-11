f5ad1c191f57c7a26e192acd1b27c3befccc9276
apterix <apterix@gmail.com>
2018-07-11 13:03:10 -0300

    add checkIfBlocked() and failMessage()

--- minecraft-dnsrotator.sh
+++ minecraft-dnsrotator.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# Print error and msg of fail
+# @params
+# 1: error code
+# @: message
+# @return
+# exit of script
+failMessage()
+{
+	error_code=$1
+	shift 1
+	error_message=$@
+
+	echo "$error_message"
+	exit $error_code
+}
+
 # Get _minecraft._tcp.domain TXT record
 # @params
 # 1: domain
 # @return
-# -1 if failed or TXT record content
+# TXT record content
 getTxtRecord()
 {
 	domain=$1
@@ -14,8 +30,7 @@ getTxtRecord()
 
 	if [ $count_result -ne "1" ]
 	then
-		# Multiple _minecraft._tcp.DOMAIN records not supported yet or tcp connection did not work
-		echo -1
+		failMessage 1 "Multiple _minecraft._tcp.DOMAIN records not supported yet or tcp connection did not work"
 	else
 		record=$(echo $result | cut -d" " -f8 | sed 's/\.$//')
 		echo $record
@@ -37,10 +52,28 @@ getJsonDomainInfo()
 
 	if [ $curl_code -ne "0" ]
 	then
-		echo -1
+		failMessage 2 "Curl failed with >> $curl_code << curl code"
 	else
 		echo $result
 	fi
 }
 
+# Verify if _minecraft._tcp.DOMAIN content is blocked
+# @parmans
+# 1: domain
+# @return
+# 1 if blocked, 0 if unblocked
+checkIfBlocked()
+{
+	domain=$1
+	domain_json_info=$(getJsonDomainInfo $domain)
+	domain_srv_record=$(getTxtRecord $domain)
+	domain_json_txt_record=$(echo $domain_json_info | jq --arg domain "$domain" '.[$domain]' | jq --arg domain2 "$domain_srv_record" '.[] | select(.domain==$domain2)')
+	domain_json_blocked=$(echo $domain_json_txt_record | jq '.blocked')
 
+	if [[ "$domain_json_blocked" == "true"]]
+		echo 1
+	else
+		echo 0
+	fi
+}
