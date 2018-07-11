df00a66911b84464324225d5687d46ea125a6b18
apterix <apterix@gmail.com>
2018-07-11 19:38:01 -0300

    add update.sh that will update minecraft-dnsrotator.sh file on system

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Did you update de git? (yes|y) to proceed, other thing to exit"
+read answer
+
+if echo $answer | egrep -qi "(y|yes)"
+then
+	cp minecraft-dnsrotator.sh /usr/bin/minecraft-dnsrotator.sh
+	chmod +x /usr/bin/minecraft-dnsrotator.sh
+fi
